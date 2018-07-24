995e819d92c721dd14fde5af057453650b3202ea
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-24 17:08:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE name = (?)
     SQL
-    DB[:conn].execute(sql, name)
+    self.new_from_db(DB[:conn].execute(sql, name))[0]
   end
 
   def update
